a888c12246421b6cfddf76a2050552138251d421
kernel_loophole <p190119@nu.edu.pk>
2024-01-01 00:36:59 -0500

    sklearn algo added

---
+++ sub_token.py
@@ -0,0 +1,10 @@
+from transformers import AutoTokenizer
+model_ckpt = "distilbert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+#example text
+text="Tokenizing text is a core task of NLP."
+encoded_text = tokenizer(text)
+print(encoded_text)
+tokens = tokenizer.convert_ids_to_tokens(encoded_text.input_ids)
+print(tokens)
+print(tokenizer.convert_tokens_to_string(tokens))
